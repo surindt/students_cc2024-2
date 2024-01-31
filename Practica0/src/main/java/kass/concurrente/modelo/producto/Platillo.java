ad8bfae53aca63ff15a10410987b55f2059748f7
Kassandra Mirael Leyva Castillo <43196753+chechenque@users.noreply.github.com>
2024-01-30 21:49:18 -0600

    Actualizacion Practica 0
    Se actualiza metodo de calcula precio con descripcion

--- Practica0/src/main/java/kass/concurrente/modelo/producto/Platillo.java
+++ Practica0/src/main/java/kass/concurrente/modelo/producto/Platillo.java
@@ -5,5 +5,15 @@ package kass.concurrente.modelo.producto;
  * @author Kassandra Mirael
  */
 public class Platillo {
+
+
+    /**
+     * Este metodo calcula el precio sobre el precio de los items, se ignora
+     * el precio base.
+     * @return El precio de la suma de cada item utilizado
+     */
+    public Double calculaPrecio(){
+        return 0.;
+    }
     
 }
