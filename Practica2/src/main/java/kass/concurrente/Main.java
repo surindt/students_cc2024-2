6c835602a3c4cd7e996a72ecc61815e1349714bc
Kassandra mirael Leyva Castillo <luis_angel_howke@ciencias.unam.mx>
2024-02-12 17:34:42 -0600

    Generacion del Archivo de constantes

--- Practica2/src/main/java/kass/concurrente/Main.java
+++ Practica2/src/main/java/kass/concurrente/Main.java
@@ -1,7 +1,9 @@
 package kass.concurrente;
 
 public class Main {
+
     public static void main(String[] args) {
+
         System.out.println("Practica 2");
     }
 }
---
+++ Practica2/src/main/java/kass/concurrente/constants/Constante.java
@@ -0,0 +1,12 @@
+package kass.concurrente.constants;
+
+/**
+ * Clase de Constantes que se utilizaran durante la practica
+ * @author Kassandra Mirael
+ * @version 1.0
+ */
+public final class Constante {
+    public static final String CONTRASENNA = "Hola"; //Modificar por su contrasenna cuando la encuentren, es la unica con mayuscula
+    public static final String LLAVE = "-87-4-5711-89-255970888-80998286839";
+    public static final Integer HILOS = 4;
+}
