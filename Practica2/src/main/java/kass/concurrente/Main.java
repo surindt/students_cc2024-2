f9943a6f0ce3b392b2ddc4825f927483e1733947
Kassandra mirael Leyva Castillo <luis_angel_howke@ciencias.unam.mx>
2024-02-13 08:53:00 -0600

    Se pone el System.nanoTime()

--- Practica2/src/main/java/kass/concurrente/Main.java
+++ Practica2/src/main/java/kass/concurrente/Main.java
@@ -9,8 +9,15 @@ import kass.concurrente.crypto.Cifrar;
 public class Main {
 
     public static void main(String[] args) throws Exception {
+        Long inicio = System.nanoTime();
         System.out.println(Cifrar.descifraC(Constante.LLAVE, Constante.CONTRASENNA));
-
+        Long fin = System.nanoTime();
+        Long total = fin-inicio;
+        System.out.println("TIEMPO TOTAL: " + nanoSegundoASegundo(total));
         System.out.println("Practica 2");
+    }
+
+    public static double nanoSegundoASegundo(Long tiempo){
+        return tiempo *1.0 * Math.pow(10,-9);
     }
 }
