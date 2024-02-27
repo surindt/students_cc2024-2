0432289dff88c246ae63bff01ec34bb7d52b0884
Kassandra Mirael Leyva Castillo <43196753+chechenque@users.noreply.github.com>
2024-02-26 18:16:19 -0600

    Update Main.java

--- Practica3/src/main/java/kass/concurrente/Main.java
+++ Practica3/src/main/java/kass/concurrente/Main.java
@@ -8,6 +8,13 @@ package kass.concurrente;
 public class Main {
     Lock lock = new ReentrantLock();
 
+    /*
+     * INSTRUCCIONES:
+     * 1.- Ya genere el lock, es un reentrantLock, investiguen que hace
+     * 2.- 
+     */
+
+
     public static void main(String[] args) {
         System.out.println("Clase que no usaremos :D");
     }
